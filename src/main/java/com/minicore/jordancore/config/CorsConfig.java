cced852eb5e772e556d7a2f53450630515ff05a3
tom-ayu <tomas.ontaneda@udla.edu.ec>
2025-10-22 19:47:16 -0500

    update cors config

--- src/main/java/com/minicore/jordancore/config/CorsConfig.java
+++ src/main/java/com/minicore/jordancore/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000","https://frontminicorejcomision.onrender.com")
+                        .allowedOrigins("https://minicorefront.netlify.app/","https://frontminicorejcomision.onrender.com")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
